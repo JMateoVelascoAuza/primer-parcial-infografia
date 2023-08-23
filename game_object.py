83c1e0c5335021871e4f5075091259aaff4f98ee
JMateoVelascoAuza <mateovelascop19@gmail.com>
2023-08-23 01:36:35 -0400

    cambios casi finales del proyecto

--- game_object.py
+++ game_object.py
@@ -1,17 +1,18 @@
 import pymunk
+import arcade
 
 ASTEROID_RADIUS = 30
 SHIP_SIZE = 10
-ASTEROID_SPEED = 100
 
 class Asteroid:
     def __init__(self, x, y, space):
         self.body = pymunk.Body(body_type=pymunk.Body.DYNAMIC)
         self.body.position = x, y
-        self.shape = pymunk.Circle(self.body, ASTEROID_RADIUS)
+        self.shape = pymunk.Circle(self.body, radius=30)
         self.shape.density = 1
         self.shape.friction = 0.5
-        space.add(self.body, self.shape)
+        self.space = space
+        self.space.add(self.body, self.shape)
 
 class Ship:
     def __init__(self, x, y, size):
@@ -25,10 +26,18 @@ class Background:
         self.center_y = height // 2
 
 class Bullet:
-    def __init__(self, x, y, velocity):
+    def __init__(self, x, y, space):
+        self.sprite = arcade.Sprite("assets/images/bala.png", scale=0.1)
         self.body = pymunk.Body(body_type=pymunk.Body.DYNAMIC)
         self.body.position = x, y
-        self.shape = pymunk.Circle(self.body, 5)
+        self.body.gravity = (0, 0)
+        self.radius = 5  # Cambio en el atributo: radius en lugar de shape
+        self.shape = pymunk.Circle(self.body, radius=self.radius)
         self.shape.density = 1
         self.shape.friction = 0.5
-        self.velocity = velocity
+        self.shape.collision_type = 2
+        self.space = space
+        self.space.add(self.body, self.shape)
+        self.sprite.center_x = x
+        self.sprite.center_y = y
+        self.body.velocity = (0, 300)
--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from game_object import Asteroid, Ship, Background, Bullet
 
 ASTEROID_RADIUS = 30
 SHIP_SIZE = 10
-ASTEROID_SPEED = 100
+ASTEROID_SPEED = 500
 
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(fullscreen=True)
         self.space = pymunk.Space()
-        self.space.gravity = 0, -1000
+        self.space.gravity = 0, 0
         self.score = 0
+        self.game_over = False
 
         self.ship = Ship(self.width // 2, self.height // 2, SHIP_SIZE)
         self.asteroid_bodies = []
@@ -23,9 +24,14 @@ class MyGame(arcade.Window):
 
         self.ship_sprite = arcade.Sprite("assets/images/xwing.png", scale=0.2)
         self.asteroid_sprite = arcade.Sprite("assets/images/asteroid.png", scale=1.0)
-        self.bullet_sprite = arcade.Sprite("assets/images/bala.png", scale=0.1)  # Add bullet image
+        self.bullet_sprite = arcade.Sprite("assets/images/bala.png", scale=0.1)
         self.background_sprite = arcade.Sprite("assets/images/fondo.png", scale=1.0)
-        self.bullet_sprites = []  # List to store bullet sprites
+        self.bullet_sprites = []
+        self.bullet_list = []
+
+    def setup(self):
+        self.score = 0
+        self.game_over = False
 
     def on_draw(self):
         arcade.start_render()
@@ -44,36 +50,60 @@ class MyGame(arcade.Window):
         for bullet_sprite in self.bullet_sprites:
             bullet_sprite.draw()
 
+        arcade.draw_text(f"Score: {self.score}", self.width // 2, self.height - 30,
+                         arcade.color.WHITE, font_size=24, anchor_x="center", anchor_y="center")
+
+        if self.game_over:
+            arcade.draw_text("Game Over", self.width // 2, self.height // 2,
+                             arcade.color.RED, font_size=48, anchor_x="center", anchor_y="center")
+
     def on_mouse_motion(self, x, y, dx, dy):
         self.ship.center_x = x
         self.ship.center_y = y
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.SPACE:
+        if key == arcade.key.SPACE and not self.game_over:
             bullet = Bullet(self.ship.center_x, self.ship.center_y, self.space)
+            self.bullet_list.append(bullet)
             self.bullet_sprites.append(bullet.sprite)
             self.space.add(bullet.body, bullet.shape)
 
     def update(self, delta_time):
+        if self.game_over:
+            return
+
         self.space.step(delta_time)
 
         asteroids_to_remove = []
-        for body in self.asteroid_bodies:
-            body.position = (body.position.x, body.position.y - ASTEROID_SPEED * delta_time)
-            if body.position.y < 0:
-                asteroids_to_remove.append(body)
-        for asteroid_body in asteroids_to_remove:
-            self.asteroid_bodies.remove(asteroid_body)
-            self.space.remove(asteroid_body, *asteroid_body.shapes)
-
         bullets_to_remove = []
-        for bullet_sprite in self.bullet_sprites:
-            bullet_sprite.center_y += 5  # Move bullet vertically
-            if bullet_sprite.center_y > self.height:
-                bullet_sprite.remove_from_sprite_lists()
-                bullets_to_remove.append(bullet_sprite)  # Remove bullet sprites
-        for bullet_sprite in bullets_to_remove:
-            self.bullet_sprites.remove(bullet_sprite)
+
+        for asteroid_body in self.asteroid_bodies:
+            asteroid_body.position = (asteroid_body.position.x, asteroid_body.position.y - ASTEROID_SPEED * delta_time)
+            if asteroid_body.position.y < 0:
+                asteroids_to_remove.append(asteroid_body)
+
+        for bullet in self.bullet_list:
+            bullet.sprite.center_x = bullet.body.position.x
+            bullet.sprite.center_y = bullet.body.position.y
+
+            if bullet.sprite.center_y > self.height:
+                bullets_to_remove.append(bullet)
+
+            for asteroid_body in self.asteroid_bodies:
+                if abs(asteroid_body.position.x - bullet.body.position.x) < ASTEROID_RADIUS and \
+                        abs(asteroid_body.position.y - bullet.body.position.y) < ASTEROID_RADIUS:
+                    self.score += 1
+                    asteroids_to_remove.append(asteroid_body)
+                    bullets_to_remove.append(bullet)
+
+        for asteroid_body in asteroids_to_remove:
+            self.space.remove(asteroid_body, *asteroid_body.shapes)
+            self.asteroid_bodies.remove(asteroid_body)
+
+        for bullet in bullets_to_remove:
+            self.bullet_list.remove(bullet)
+            self.space.remove(bullet.body, bullet.shape)
+            bullet.sprite.remove_from_sprite_lists()  # Eliminar el sprite de la bala
 
         self.asteroid_creation_time += delta_time
         if self.asteroid_creation_time >= self.asteroid_creation_interval:
@@ -82,16 +112,11 @@ class MyGame(arcade.Window):
             self.asteroid_creation_time = 0
 
         for asteroid_body in self.asteroid_bodies:
-            for bullet_sprite in self.bullet_sprites:
-                if abs(asteroid_body.position.x - bullet_sprite.center_x) < ASTEROID_RADIUS and \
-                        abs(asteroid_body.position.y - bullet_sprite.center_y) < ASTEROID_RADIUS:
-                    self.score += 1
-                    self.space.remove(asteroid_body, asteroid_body.shapes)
-                    self.asteroid_bodies.remove(asteroid_body)
-                    bullet_sprite.remove_from_sprite_lists()
-                    if self.score % 10 == 0:
-                        print("¡La nave ha destruido 10 asteroides!")
+            if abs(asteroid_body.position.x - self.ship.center_x) < ASTEROID_RADIUS and \
+                    abs(asteroid_body.position.y - self.ship.center_y) < ASTEROID_RADIUS:
+                self.game_over = True
 
 if __name__ == "__main__":
     game = MyGame()
+    game.setup()
     arcade.run()
